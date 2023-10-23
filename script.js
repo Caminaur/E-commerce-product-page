4fea6b37d00cd5ecc11f565d49d218754530d139
JulianC <jacaminaur@gmail.com>
2023-10-23 13:34:26 +0200

    mobile Menu

--- script.js
+++ script.js
@@ -10,8 +10,15 @@
 
   const lightbox = document.getElementById('lightbox');
   const lightboxImg = document.getElementById('lightbox-image');
+  const prevLightbox = document.getElementById('prev-lightbox');
   const prev = document.getElementById('prev');
+  const nextLightbox = document.getElementById('next-lightbox');
   const next = document.getElementById('next');
+  const closeBtn = document.getElementById('lightbox-close');
+
+  const menuMobile = document.querySelector('.mobile-menu');
+  const menuBtn = document.getElementById('menu-btn');
+  const menuClose = document.getElementById('menu-close');
 
   plus.addEventListener('click',(e)=>{
     value =  (parseInt(amount.value) + 1);
@@ -33,10 +40,10 @@
       resetSelector();
       image.dataset.imageSelected = true;
       if (image.id.includes('b')) {
-        imagen1 = document.getElementById(image.id.replace('b',''));
+        let imagen1 = document.getElementById(image.id.replace('b',''));
         imagen1.dataset.imageSelected = true;
       } else{
-        imagen2 = document.getElementById(image.id.concat('b'));
+        let imagen2 = document.getElementById(image.id.concat('b'));
         imagen2.dataset.imageSelected = true;
       }
       
@@ -57,14 +64,22 @@
     lightboxImg.src = mainImage.src;
   });
 
+  closeBtn.addEventListener('click',(e)=>{
+    if(e.target !== e.currentTarget) return;
+    lightbox.dataset.status = 'inactive';
+  });
+
   lightbox.addEventListener('click',(e)=>{
     if(e.target !== e.currentTarget) return;
     lightbox.dataset.status = 'inactive';
   });
 
 
+  prevLightbox.addEventListener('click', previousImage);
+  nextLightbox.addEventListener('click', nextImage);
   prev.addEventListener('click', previousImage);
   next.addEventListener('click', nextImage);
+
   document.onkeydown = (e) =>{
     if (lightbox.dataset.status!=='active')return;
     if(e.key==="ArrowRight"){
@@ -74,6 +89,15 @@
       previousImage();
     }
   };
+
+  menuClose.addEventListener('click',()=>{
+    menuMobile.dataset.active = 'false'; 
+  });
+  
+  menuBtn.addEventListener('click',()=>{
+    menuMobile.dataset.active = true; 
+  });
+
 
 
   function previousImage(){
